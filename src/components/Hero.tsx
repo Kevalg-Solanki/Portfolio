8d0570201ce6ececb7495ca1cb10fa5dac675020
Kevalg-Solanki <kevalg1950@gmail.com>
2026-06-16 17:53:24 +0530

    change: 50+ APIs to 1 product

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -187,10 +187,10 @@ export default function Hero() {
 
 							<div>
 								<h3 className="text-2xl font-bold text-slate-900">
-									50+
+								1
 								</h3>
 								<p className="text-slate-500 text-sm">
-									APIs Developed
+								 Live Product
 								</p>
 							</div>
 
